93c1102e4f259b50f9a967d4a5a62858824238d5
mahesmvp <mahesmvp94@gmail.com>
2019-11-08 16:23:13 +1100

    Experimenting with v8

--- core.js
+++ core.js
@@ -1,9 +1,11 @@
+// -------------------Path Package--------------------
 const path = require("path")
 
 const dirUploads = path.join(__dirname,"www","files","uploads");
 
 console.log(dirUploads);
 
+// -------------------Core Package--------------------
 const util = require("util");
 
 util.log(path.basename(__filename))
@@ -11,5 +13,10 @@ util.log("The name of the file is ^")
 
 util.log(path.basename(__dirname))
 util.log("The directory is ^")
+
+// -------------------v8 Package--------------------
+const v8 = require("v8");
+
+util.log(v8.getHeapStatistics());
 
 
