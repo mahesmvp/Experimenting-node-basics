be89ab92caadbf97361e864a0ba44d0bf37a8713
mahesmvp <mahesmvp94@gmail.com>
2019-11-08 16:29:22 +1100

    Experimenting with readline

---
+++ ask.js
@@ -0,0 +1,10 @@
+const readLine = require("readline");
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("How do you like node? ", answer =>{
+    console.log(`Your answer is: ${answer}`)
+});
