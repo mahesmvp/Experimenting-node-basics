41abadd572e81fdab1d74e97e44092425e0ede01
mahesmvp <mahesmvp94@gmail.com>
2019-11-08 16:19:54 +1100

    Experimenting with Core package

--- core.js
+++ core.js
@@ -4,3 +4,12 @@ const dirUploads = path.join(__dirname,"www","files","uploads");
 
 console.log(dirUploads);
 
+const util = require("util");
+
+util.log(path.basename(__filename))
+util.log("The name of the file is ^")
+
+util.log(path.basename(__dirname))
+util.log("The directory is ^")
+
+
